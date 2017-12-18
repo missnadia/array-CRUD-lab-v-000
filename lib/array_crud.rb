51b141ffb761e860353ff61f6f24cc6a7a1b9daf
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-18 02:32:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  element = array.push("arrays!")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  element = array.unshift("wow")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
